75a95302ae650a2caa106eea7dfee36ff9c1d823
Matt Kilcup <nevermindthelabel@gmail.com>
2020-07-05 00:14:20 -0700

    created http server

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log('Hey Deno');
+import { serve } from "https://deno.land/std@0.60.0/http/server.ts";
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+for await (const req of s) {
+  req.respond({ body: 'Hey Deno' });
+}
